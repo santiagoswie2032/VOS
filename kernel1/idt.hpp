cba48063c1bb5ec53fb708a411b3296fee0b9a1a
santiago <vikalpbordekar@gmail.com>
2026-02-20 16:17:48 +0000

    adding kernel files

---
+++ kernel1/idt.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <stdint.h>
+
+struct IDTEntry {
+    uint16_t offset_low;
+    uint16_t selector;
+    uint8_t  zero;
+    uint8_t  type_attr;
+    uint16_t offset_high;
+} __attribute__((packed));
+
+struct IDTPointer {
+    uint16_t limit;
+    uint32_t base;
+} __attribute__((packed));
