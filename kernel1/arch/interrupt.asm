54e372a9d4b05ca9439da8aa7db3a4d038eeee26
Vikalp Bordekar <vikalpbordekar@gmail.com>
2026-03-07 00:00:45 +0530

    Fix  interrupt.asm

--- kernel1/arch/interrupt.asm
+++ kernel1/arch/interrupt.asm
@@ -8,8 +8,8 @@ keyboard_asm_handler:
     pusha              ; save registers
 
     call keyboard_handler
-
-    mov al, 0x20       ; send EOI to PIC
+  
+    mov al, 0x20       ; send EOI to PIC 
     out 0x20, al
 
     popa               ; restore registers
