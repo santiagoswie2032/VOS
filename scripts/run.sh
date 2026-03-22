dd09986682322d2d7cd9b4944bf28369bbe3d237
santiago <vikalpbordekar@gmail.com>
2026-03-22 16:59:54 +0000

    installing cross compilers and updating run scripts to linux commands instead of elf(s)

--- scripts/run.sh
+++ scripts/run.sh
@@ -13,15 +13,15 @@ mkdir -p iso/boot/grub
 nasm -f bin bootloader/boot.asm -o build/boot.bin
 
 
-i686-elf-g++ -ffreestanding -c kernel/kernel.cpp -o build/kernel.o
-i686-elf-g++ -ffreestanding -c kernel/arch/idt.cpp -o build/idt.o
-i686-elf-g++ -ffreestanding -c kernel/drivers/keyboard.cpp -o build/keyboard.o
+i686-linux-g++ -ffreestanding -c kernel/kernel.cpp -o build/kernel.o
+i686-linux-g++ -ffreestanding -c kernel/arch/idt.cpp -o build/idt.o
+i686-linux-g++ -ffreestanding -c kernel/drivers/keyboard.cpp -o build/keyboard.o
 
 
 nasm -f elf32 kernel/arch/interrupt.asm -o build/interrupt.o
 
 
-i686-elf-ld -T linker.ld -o build/kernel.bin \
+i686-linux-ld -T linker.ld -o build/kernel.bin \
 build/kernel.o build/idt.o build/keyboard.o build/interrupt.o
 
 cat > iso/boot/grub/grub.cfg << EOF
